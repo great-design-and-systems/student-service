f9e6e66946bff2f1c733f80625cab0859a62e022
rickzx98 <rickzx98@gmail.com>
2016-07-22 04:22:57 +0800

    added log4js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var Server = require('./src/config/server');
 var StudentResource = require('./src/boundary/student-resource');
 var express = require('express');
 var app = express();
+var LoggerServer = require('./src/config/logger-server');
 
-(function() {
+(function () {
     new Database();
     new Server(app);
     new StudentResource(app);
+    new LoggerServer(app);
 })();
 
 module.exports = app;
