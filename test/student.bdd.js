5e483dd19fee0d1767de9e3de8a2ab5be149997d
rickzx98 <rickzx98@gmail.com>
2016-07-24 16:46:25 +0800

    updates

--- test/student.bdd.js
+++ test/student.bdd.js
@@ -3,14 +3,17 @@ var Database = require('./config/database');
 var sinon = require('sinon');
 var chai = require('chai');
 var expect = chai.expect;
-describe('Student Service BDD', function() {
+var CONNECT_TEST_TIMEOUT = process.env.CONNECT_TEST_TIMEOUT || 50000;
+
+describe('Student Service BDD', function () {
     var db = new Database();
 
-    beforeEach(function(done) {
+    beforeEach(function (done) {
+        this.timeout(CONNECT_TEST_TIMEOUT)
         return db.connect(done);
     });
 
-    describe('GIVEN: I have studentId, firstname, middlename, lastname, dateOfBirth, gender, address, barcode, contactNo, department and level', function() {
+    describe('GIVEN: I have studentId, firstname, middlename, lastname, dateOfBirth, gender, address, barcode, contactNo, department and level', function () {
         var studentId = '1234567890';
         var firstname = 'Analyn';
         var lastname = 'Flores';
@@ -24,7 +27,7 @@ describe('Student Service BDD', function() {
         var level = 'First Year College';
         var studentForm = {};
 
-        beforeEach(function() {
+        beforeEach(function () {
             studentForm.studentId = studentId;
             studentForm.firstname = firstname;
             studentForm.lastname = lastname;
@@ -38,47 +41,47 @@ describe('Student Service BDD', function() {
             studentForm.level = level;
         });
 
-        describe('WHEN: saving student', function() {
+        describe('WHEN: saving student', function () {
             var savedResult;
             var studentId;
-            beforeEach(function(done) {
-                Student.create(studentForm, function(err, studentSavedResult) {
+            beforeEach(function (done) {
+                Student.create(studentForm, function (err, studentSavedResult) {
                     savedResult = studentSavedResult;
                     studentId = studentSavedResult.studentId;
                     done();
                 });
             });
 
-            it('THEN: response is student profile', function() {
+            it('THEN: response is student profile', function () {
                 expect(!!savedResult).to.equal(true);
             });
 
-            describe('GIVEN: I have studentId', function() {
-                describe('WHEN: getting studentProfile', function() {
+            describe('GIVEN: I have studentId', function () {
+                describe('WHEN: getting studentProfile', function () {
                     var studentProfile;
 
-                    beforeEach(function(done) {
-                        Student.getProfileByStudentId(studentId, function(err, studentProfileResult) {
+                    beforeEach(function (done) {
+                        Student.getProfileByStudentId(studentId, function (err, studentProfileResult) {
                             studentrofile = userProfileResult;
                             done();
                         });
                     });
 
-                    it('THEN: student profile is retrieved', function() {
+                    it('THEN: student profile is retrieved', function () {
                         expect(!!studentrofile).to.equal(true);
                     });
 
                 });
-                describe('WHEN: removing student', function() {
+                describe('WHEN: removing student', function () {
                     var message;
-                    beforeEach(function(done) {
-                        Student.removeUser(studentId, function(err, result) {
+                    beforeEach(function (done) {
+                        Student.removeUser(studentId, function (err, result) {
                             message = result.message;
                             done();
                         });
                     });
 
-                    it('THEN: student and student profile are removed', function() {
+                    it('THEN: student and student profile are removed', function () {
                         expect(!!message).to.equal(true);
                         expect(message).to.equal('Student has been removed.');
                     });
@@ -88,7 +91,7 @@ describe('Student Service BDD', function() {
         });
     });
 
-    afterEach(function(done) {
+    afterEach(function (done) {
         return db.disconnect(done);
     });
 });
